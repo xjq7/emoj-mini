b43666f690030f395c63db051905aa170b1420fe
xjq <lebnizjoan@gmail.com>
2022-03-25 00:25:28 +0800

    init

---
+++ src/app.tsx
@@ -0,0 +1,31 @@
+import { Component } from 'react'
+import { Provider } from 'mobx-react'
+
+import counterStore from './store/counter'
+
+import './app.scss'
+
+const store = {
+  counterStore
+}
+
+class App extends Component {
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // this.props.children 就是要渲染的页面
+  render () {
+    return (
+      <Provider store={store}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export default App
