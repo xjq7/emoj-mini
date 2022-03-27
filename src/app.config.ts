0a3eacf00bee5cf093791d43f6a5c6508e9c2668
xjq <lebnizjoan@gmail.com>
2022-03-27 14:18:12 +0800

    feat: tabbar配置

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,5 +1,7 @@
+import themeMap from './utils/theme';
+
 export default defineAppConfig({
-  pages: ['pages/home/index', 'pages/my/index', 'pages/login/index', 'pages/emojDetail/index'],
+  pages: ['pages/home/index', 'pages/my/index', 'pages/login/index', 'pages/emojDetail/index', 'pages/emojPack/index'],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
@@ -7,17 +9,25 @@ export default defineAppConfig({
     navigationBarTextStyle: 'black',
   },
   // lazyCodeLoading: 'requiredComponents',
-  // tabBar: {
-  //   color: '#ccccc',
-  //   selectedColor: themeMap.$Primary,
-  //   backgroundColor: themeMap.$White,
-  //   position: 'bottom',
-  //   borderStyle: 'black',
-  //   list: [
-  //     {
-  //       pagePath: 'pages/home/index',
-  //       text: '首页',
-  //     },
-  //   ],
-  // },
+  tabBar: {
+    color: '#ccccc',
+    selectedColor: themeMap.$Primary,
+    backgroundColor: themeMap.$White,
+    position: 'bottom',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/home/index',
+        text: '首页',
+        iconPath: './assets/home.png',
+        selectedIconPath: './assets/home_selected.png',
+      },
+      {
+        pagePath: 'pages/emojPack/index',
+        text: '表情包',
+        iconPath: './assets/emojPack.png',
+        selectedIconPath: './assets/emojPack_selected.png',
+      },
+    ],
+  },
 });
---
+++ src/pages/emojPack/index.tsx
@@ -0,0 +1,8 @@
+import { View } from '@tarojs/components';
+import styles from './index.module.scss';
+
+const Component = () => {
+  return <View></View>;
+};
+
+export default Component;
